55fdd6b9085556ad496e7d14628fd6ab214717dd
chenzhilin <chenzhilin@17m.club>
2017-03-09 11:47:50 +0800

    sencend

---
+++ CZLTools/CZLTools/Tools/TempModel.h
@@ -0,0 +1,14 @@
+//
+//  TempModel.h
+//  CZLTools
+//
+//  Created by apple on 2017/3/9.
+//  Copyright © 2017年 xingrui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TempModel : NSObject
+
+@property (nonatomic,assign)BOOL isModel;
+@end
---
+++ CZLTools/CZLTools/Tools/TempModel.m
@@ -0,0 +1,13 @@
+//
+//  TempModel.m
+//  CZLTools
+//
+//  Created by apple on 2017/3/9.
+//  Copyright © 2017年 xingrui. All rights reserved.
+//
+
+#import "TempModel.h"
+
+@implementation TempModel
+
+@end
